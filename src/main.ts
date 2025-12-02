7d511592e61210b87a67e9bf6ed3828c8f5837bd
Donny <tanadornrach@gmail.com>
2025-12-02 15:52:04 +0700

    enable cors for developmental purpose

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN?.split(",") ?? [
+      "http://localhost:3000",
+      "http://localhost:5500",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 8085);
 }
 bootstrap();
