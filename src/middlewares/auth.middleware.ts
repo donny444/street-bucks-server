0694b6c8859bbcf9b5e96d197979ce7653e9a48e
Donny <tanadornrach@gmail.com>
2026-01-17 16:26:05 +0700

    init `AdminAction` middleware to validate admin access

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,6 +2,8 @@ import { Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 import * as bcrypt from "bcryptjs";
 import { PrismaClient, $Enums } from "../../prisma/client";
+
+import { EditMenuDto } from "../dtos/menu.dto";
 import { EditUserDto, RemoveUserDto, AuthDto } from "../dtos/user.dto";
 
 @Injectable()
@@ -94,6 +96,58 @@ export class AuthorizeUser implements NestMiddleware {
         return res
           .status(401)
           .json({ message: "Only managers are authorized" });
+      }
+
+      return next();
+    } catch (error) {
+      console.error("Error in `AuthorizeUser` middleware:", error);
+      return res.status(500).json({ message: "Failed to authorize user." });
+    }
+  }
+}
+
+export class AdminAction implements NestMiddleware {
+  constructor(private readonly prisma: PrismaClient) {}
+  async use(
+    req: Request<any, any, EditMenuDto>,
+    res: Response,
+    next: NextFunction
+  ) {
+    const { email, password } = req.body.editor;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password of editor are required.",
+      });
+    }
+
+    try {
+      const editor = await this.prisma.user.findUnique({
+        select: {
+          role: true,
+          password: true,
+        },
+        where: {
+          email,
+        },
+      });
+      if (!editor) {
+        return res
+          .status(404)
+          .json({ message: "Editor not found with the provided email." });
+      }
+
+      const correctPassword = await bcrypt.compare(password, editor.password);
+      if (!correctPassword) {
+        return res
+          .status(401)
+          .json({ message: "Incorrect password for editor." });
+      }
+
+      if (editor.role !== $Enums.Role.ADMINISTRATOR) {
+        return res
+          .status(401)
+          .json({ message: "Only administrators are authorized" });
       }
 
       return next();
